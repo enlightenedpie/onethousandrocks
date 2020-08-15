30564e8e3259ceaf30192e67ef1686d5810467a8
David Paul Crouch <8129146+enlightenedpie@users.noreply.github.com>
2020-08-14 21:15:06 -0500

    changes to touch events

--- src/otr.js
+++ src/otr.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './otr.css'
 
-const OneRock = ({x,y,count,deg}) => {
+const OneRock = ({ror,x,y,count,deg}) => {
+    let [pressed,setPressed] = useState(false);
     return (
-        <div id={"otr"+count} className={"onerock"} style={{
-            left: x+'%',
-            top: y+'%',
-            zIndex: count,
-            transform: "rotate3d(0,0,1,"+deg+"deg) translate3d(-50%,-50%,0)"
-        }}>
+        <div id={"otr"+count} className={"onerock"}
+            style={{
+                left: x+'%',
+                top: y+'%',
+                zIndex: count,
+                transform: "rotate3d(0,0,1,"+deg+"deg) translate3d(-50%,-50%,0)" + (pressed ? ' scale3d(1.1,1.1,1)' : '')
+            }}
+            onMouseDown={(e) => {setPressed(true);}}
+            onTouchStart={(e) => {setPressed(true);}}
+            onMouseUp={(e) => {setPressed(false);}}
+            onTouchEnd={(e) => {setPressed(false);}}
+        >
             <img alt="" src={count === 30 ? "/ima/thirtyrock.png" : "/ima/onerock.png"}/>
         </div>
     )
 }
 
 export default class OTR extends React.Component {
-    state = {
-        counter: 0,
-        els: [null]
+    constructor() {
+        super()
+        this.state = {
+            counter: 0,
+            els: [null]
+        }
+
+        this.removeOneRock = this.removeOneRock.bind(this);
+        this.makeOneRock = this.makeOneRock.bind(this);
+    }
+
+    removeOneRock(count) {
+        this.setState(state => {
+            let newEls = state.els
+            newEls.splice(count,1)
+            return {
+                counter: --state.counter,
+                els: newEls
+            }
+        })
     }
 
     makeOneRock() {
@@ -33,7 +57,7 @@ export default class OTR extends React.Component {
         window.setTimeout(() => {
             this.setState(state => {
                 let newEls = state.els
-                newEls.push(<OneRock key={state.counter+1} count={state.counter+1} {...obj}/>)
+                newEls.push(<OneRock ror={this.removeOneRock} key={state.counter+1} count={state.counter+1} {...obj}/>)
                 return {
                     counter: ++state.counter,
                     els: newEls
